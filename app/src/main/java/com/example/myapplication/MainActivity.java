fbd9aaac4b537dbed9cf34ab56896d4c29259610
AhmedElatreby <ahmedsport@hotmail.com>
2023-04-28 17:15:16 +0100

    fc

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onBtnClick(View view) {
+        EditText edTxtName = findViewById(R.id.edTxtName);
+        TextView txtHello = findViewById(R.id.txtMessage);
+
+        txtHello.setText( "Hello " + edTxtName.getText().toString());
+    }
+
+}
